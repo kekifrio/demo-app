86099740644371941e8bcf7a28238969c01edc3e
kekifrio <kekifrio@gmail.com>
2023-09-23 19:47:46 -0500

    filter users functionality ready

--- src/components/UserCards.tsx
+++ src/components/UserCards.tsx
@@ -1,22 +1,24 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 interface personsArray {
-  name: String;
-  national_id: String;
-  phone_number: String;
-  address: String;
-  salary: String;
+  name: string;
+  national_id: string;
+  phone_number: string;
+  address: string;
+  salary: string;
 }
+
 function UserCard() {
   const [persons, setPersons] = useState<personsArray[]>([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filter, setFilter] = useState("");
 
   const getAllPersons = async () => {
     try {
       const response = await axios.get("/api/person/getAll");
       setPersons(response.data.data);
-
       setLoading(false);
     } catch (error) {
       console.error(error);
@@ -25,30 +27,82 @@ function UserCard() {
 
   useEffect(() => {
     getAllPersons();
-    console.log(persons);
   }, []);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) => {
+    if (searchTerm === "") {
+      return true; // Return all persons if search term is empty
+    }
+
+    // Filter based on selected attribute
+    if (filter === "name") {
+      return person.name.toLowerCase().startsWith(searchTerm.toLowerCase());
+    } else if (filter === "national_id") {
+      return person.national_id
+        .toLowerCase()
+        .startsWith(searchTerm.toLowerCase());
+    } else if (filter === "phone_number") {
+      return person.phone_number
+        .toLowerCase()
+        .startsWith(searchTerm.toLowerCase());
+    } else if (filter === "address") {
+      return person.address.toLowerCase().startsWith(searchTerm.toLowerCase());
+    } else if (filter === "salary") {
+      return person.salary.toLowerCase().startsWith(searchTerm.toLowerCase());
+    }
+
+    return true; // Return all persons if no filter is selected
+  });
 
   return (
     <div>
+      <div>
+        <input
+          className="text-black"
+          type="text"
+          value={searchTerm}
+          onChange={handleSearch}
+          placeholder="Search..."
+        />
+        <select
+          className="text-black"
+          value={filter}
+          onChange={handleFilterChange}
+        >
+          <option value="">All</option>
+          <option value="name">Name</option>
+          <option value="national_id">National ID</option>
+          <option value="phone_number">Phone Number</option>
+          <option value="address">Address</option>
+          <option value="salary">Salary</option>
+        </select>
+      </div>
       {loading ? (
         <p>Loading...</p>
       ) : (
         <ul>
-          {persons.length > 1 &&
-            persons.map((person, i) => (
-              <li key={i}>
-                <strong>Name:</strong> {person.name}
-                <br />
-                <strong>National ID:</strong> {person.national_id}
-                <br />
-                <strong>Phone Number:</strong> {person.phone_number}
-                <br />
-                <strong>Address:</strong> {person.address}
-                <br />
-                <strong>Salary:</strong> {person.salary}
-                <br />
-              </li>
-            ))}
+          {filteredPersons.map((person, i) => (
+            <li key={i}>
+              <strong>Name:</strong> {person.name}
+              <br />
+              <strong>National ID:</strong> {person.national_id}
+              <br />
+              <strong>Phone Number:</strong> {person.phone_number}
+              <br />
+              <strong>Address:</strong> {person.address}
+              <br />
+              <strong>Salary:</strong> {person.salary}
+              <br />
+            </li>
+          ))}
         </ul>
       )}
     </div>
